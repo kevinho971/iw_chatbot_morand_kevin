8a50c2ae007fe198612d5cfdc68f8b27a17fb49d
Kevin <morand.kevin55@gmail.com>
2017-10-11 12:38:23 +0200

    Bot de résa

--- tp2/app.js
+++ tp2/app.js
@@ -21,23 +21,85 @@ server.post('/api/messages', connector.listen());
 var bot = new builder.UniversalBot(connector, [
     function (session){
         session.beginDialog('greetings');
-    }
+    },
 ]);
-
+// Dialogue par défaut
 bot.dialog('greetings',[
     function(session){
         session.beginDialog('askName');
     },
+    function(session){
+        session.beginDialog('resa');
+    },
+]);
+
+// Dialogue de demande de nom
+bot.dialog('askName',[
+    function(session){
+        builder.Prompts.text(session, 'Bienvenue Dans BotResa quel est ton prénom ?');
+    },
     function(session, results){
-        session.endDialog('Hello %s!', results.response);
+        session.endDialog('Bonjour %s!', results.response);
     }
 ]);
 
-bot.dialog('askName',[
- function(session){
-     builder.Prompts.text(session, 'Hi! What is your name ?');
- },
- function(session, results){
-     session.endDialogWithResult(results);
- }
+//Dialogue de Résevation
+bot.dialog('resa', [
+    function (session) {
+        session.beginDialog('stepResa');
+    },
+    function (session, results) {
+        session.send(`Votre réservation : <br/>
+        Date : ${results.Date} <br/>
+        Nombre de personne : ${results.nbPeople} <br/>
+        Nom de la réservation : ${results.resaName}`);
+    }
 ]);
+
+
+// Dialogue étape de résa
+bot.dialog('stepResa', [
+    function (session) {
+        builder.Prompts.text(session, 'Pour quelle date souhaitez vous une reservation ?');
+    },
+    function (session, results) {
+        // Svg de la date
+        session.dialogData.Date = results.response;
+        builder.Prompts.number(session, 'Pour combien de personne ?');
+    },
+    function (session, results) {
+        // Svg de du nombre de personne
+        session.dialogData.nbPeople = results.response;
+        builder.Prompts.text(session, 'Quel est le nom de la personne qui réserve ?');
+    },
+    function (session, results) {
+        // Svg du nom de reservation
+        session.dialogData.resaName = results.response;
+        var finalResults = {
+            Date: session.dialogData.Date,
+            nbPeople: session.dialogData.nbPeople,
+            resaName: session.dialogData.resaName
+        }
+        session.endDialogWithResult(finalResults);
+    }
+]);
+
+
+/* 
+State userData pour stocker le nom du user
+
+Hello 
+Bienvenue dans le Bot Résa
+AskName
+
+3 prompts
+    Quelle date
+    Combien de personnes
+    A quel nom
+
+Récapitulatif résa
+    Date
+    Nb personne
+    Le nom de reservation
+
+*/
